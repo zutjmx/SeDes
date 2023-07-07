14c3b540848c9b522a8a7ffad6da98f320f74175
Jesus Zuñiga <jesuszt@hotmail.com>
2023-07-07 12:12:36 -0600

    Se agrega Faker para .net core y se crea archivo Modelos.cs

---
+++ models/Modelos.cs
@@ -0,0 +1,7 @@
+namespace SeDes {
+    public class Product {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
